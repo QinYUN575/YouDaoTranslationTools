edd1c613775e6c2d109a4ca08e48098b128926c9
TsMax <QinYUN575@Gmail.com>
2020-09-24 12:58:14 +0800

    Update README and Add unit_test

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # YouDaoTranslationTools
 YouDaoTranslationTools - 有道翻译工具
+
+
+## 概述（Description）：
+
+基于[**有道智云**](http://ai.youdao.com/)开发的翻译小工具
+
+
+> 使用 Python3 开发
+
+**特性（Feature）：**
+
+- [X] 支持文本翻译
+
+
+待实现功能 TODO:
+
+- [] 支持文档批量翻译
+- [] 支持 TTS
+
+
+
+**相关资源（Resources）：**
+
+- [有道智云](http://ai.youdao.com/)
+- [有道智云·AI 开放平台 帮助与文档](https://ai.youdao.com/doc.s#guide)
+    - 文本翻译服务
+
+使用开源库：
+
+- [rich](https://github.com/willmcgugan/rich)
---
+++ unit test/Text_translation_service.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import sys
+import uuid
+import requests
+import hashlib
+import time
+from importlib import reload
+from rich import print
+from rich.console import Console
+import time
+import json
+
+console = Console()
+reload(sys)
+
+YOUDAO_URL = 'https://openapi.youdao.com/api'
+APP_KEY = '您的应用ID'
+APP_SECRET = '您的应用密钥'
+FROM_LANG = 'zh-CHS'
+TO_LANG ='en'
+
+def encrypt(signStr):
+    hash_algorithm = hashlib.sha256()
+    hash_algorithm.update(signStr.encode('utf-8'))
+    return hash_algorithm.hexdigest()
+
+
+def truncate(q):
+    if q is None:
+        return None
+    size = len(q)
+    return q if size <= 20 else q[0:10] + str(size) + q[size - 10:size]
+
+
+def do_request(data):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    return requests.post(YOUDAO_URL, data=data, headers=headers)
+
+
+def connect():
+    q = "中文测试文本"
+
+    data = {}
+    data['from'] = FROM_LANG
+    data['to'] = TO_LANG
+    data['signType'] = 'v3'
+    curtime = str(int(time.time()))
+    data['curtime'] = curtime
+    salt = str(uuid.uuid1())
+    signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET
+    sign = encrypt(signStr)
+    data['appKey'] = APP_KEY
+    data['q'] = q
+    data['salt'] = salt
+    data['sign'] = sign
+
+    # console.log('data: ' + str(data))
+
+    response = do_request(data)
+    # print('response: ' + str(response.content.decode("utf-8")))
+    # console.log('response: ' + str(response.content))
+
+    contentType = response.headers['Content-Type']
+    console.log('ContentType: ' + contentType)
+
+    if contentType == "audio/mp3":
+        millis = int(round(time.time() * 1000))
+        filePath = "合成的音频存储路径" + str(millis) + ".mp3"
+        fo = open(filePath, 'wb')
+        fo.write(response.content)
+        fo.close()
+    if contentType == 'application/json;charset=UTF-8':
+        console.print('源文本:' , q)
+        response_json = json.loads(response.content.decode("utf-8"))
+        console.print('目标文本:' , response_json.get('translation')[0])
+        pass
+    else:
+        console.log('Fail', response.content)
+
+if __name__ == '__main__':
+    connect()
